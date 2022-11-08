7dc298df11ef4bfb60e64ab285d2549a7094110c
zimasile <jaydenngcukana@gmail.com>
2022-11-08 10:17:30 +0200

    first

--- js/code.js
+++ js/code.js
@@ -1,3 +1,46 @@
-function madeBy() {
-    alert("Hello My name is Zimasile Ngcukana!!");
+// function madeBy() {
+//     alert("Hello My name is Zimasile Ngcukana!!");
+// }
+// let userName = 'Zimasile';
+// let userAge = "19";
+// let user = 
+// console.log(userName);
+// console.log(userAge);
+// console.log(typeof userName);
+/* let person = {
+    name: 'Jamie',
+    surname: 'Kinnear',
+    age: 26
+};
+console.log(person);
+
+function greet() {
+    console.log('Hello, Jamie')
 }
+
+greet() */
+
+// Revision
+
+// numb1 = 8;
+// let userName = 'Zimasile';
+// const vat = .16;
+// var surname = "Ngcukana";
+// var data;
+// console.log(suname);
+/* let numb1 = 9+2+'Zimasile';
+console.log(numb1) */
+// 5
+// let numb1 = prompt("Please enter numb1: ");
+// 5
+// let numb2 = prompt("Please enter numb2: ");
+// let addition = numb1 + numb2;
+// console.log(addition);
+// let numb1 = 4;
+// let numb2 = 3;
+// let product = eval(`${numb1}*${numb2}`);
+// console.log(product);
+
+let firstName = "Zimasile";
+let age = 19;
+console.log(`My name is ${firstName} and I am ${age} years old`)
